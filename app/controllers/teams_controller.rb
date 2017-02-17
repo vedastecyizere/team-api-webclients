99b086dbd97feaf866d3e101db571f8a03f17d5e
Vedaste Cyizere <revedaste@gmail.com>
2017-02-17 10:05:54 +0200

    using the dot env gem to DRY a code

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,27 @@
 class TeamsController < ApplicationController
 
   def show 
-    @team = Unirest.get("http://localhost:3000/api/v1/teams/#{params[:id]}.json").body
+    @team = Unirest.get("#{ENV['DOMAIN']}/#{params[:id]}.json").body
   end 
 
+  def new 
+    
+  end
+
+  def create
+    @team = Unirest.post("#{ENV['DOMAIN']}.json", :headers => {"Accept" => "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state => params[:state], :sport => params[:sport]}).body 
+    redirect_to "/teams/#{@team['id']}"
+  end
+
+  def update 
+    @team = Unirest.patch("#{ENV['DOMAIN']}.json", :headers => {"Accept" => "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state => params[:state], :sport => params[:sport]}).body
+    
+  end
+
+
+
   def index 
-    @teams = Unirest.get("http://localhost:3000/api/v1/teams.json").body
+    @teams = Unirest.get("#{ENV['DOMAIN']}.json").body
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+post "/teams", to: 'teams#create'
+get "/teams/new", to: 'teams#new'
 get "/teams/:id", to: 'teams#show'
 get "/teams", to: 'teams#index'
 
